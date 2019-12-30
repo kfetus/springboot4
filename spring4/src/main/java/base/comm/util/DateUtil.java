277e4a4d89e63a60397913393c3c5c1b6bac7b66
naru <naru@DESKTOP-5J75997>
2019-12-30 16:29:02 +0900

    주석 추가

--- spring4/src/main/java/base/comm/util/DateUtil.java
+++ spring4/src/main/java/base/comm/util/DateUtil.java
@@ -3,9 +3,18 @@ package base.comm.util;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
-
+/**
+ * 일자 관련 유틸 클래스
+ * 
+ * @author naru
+ *
+ */
 public class DateUtil {
 
+	/**
+	 * 오늘 일자 
+	 * @return ex:20191231
+	 */
 	public static String getToday() {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		Calendar calendar = Calendar.getInstance(Locale.KOREA); 
